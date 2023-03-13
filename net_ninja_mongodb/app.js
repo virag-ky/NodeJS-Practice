c2d77dc4351ba06cc2d9ec5bc99a87d7e6a54f75
Virag Kormoczy <vkormoczy@gmail.com>
2023-03-13 13:55:24 +0200

    Fetch a single document

--- net_ninja_mongodb/app.js
+++ net_ninja_mongodb/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ObjectId } = require('mongodb');
 const { connectToDb, getDb } = require('./db');
 
 // initialize the app by invoking the express function
@@ -28,5 +29,18 @@ app.get('/books', (req, res) => {
     })
     .catch((err) => {
       res.status(500).json({ error: 'Could not fetch the documents' });
+    });
+});
+
+app.get('/books/:id', (req, res) => {
+  const id = new ObjectId(req.params.id);
+
+  db.collection('books')
+    .findOne({ _id: id })
+    .then((doc) => {
+      res.status(200).json(doc);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Could not find the document' });
     });
 });
